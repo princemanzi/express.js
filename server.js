ab4bdd5994c0754d908fd00facd1e732fea8d881
Prince Manzi <manziprincemp@gmail.com>
2025-02-23 19:02:52 +0200

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require ('express')
+const express = require("express");
 
-const App = express()
-App.set("view engine", "ejs")
+const App = express();
+App.set("view engine", "ejs");
 
-App.get('/', (req, res) => {
-    console.log("Here")
-    // res.download("server.js")
-    res.render("index", { text:"World"})
-})
+App.get("/", (req, res) => {
+  console.log("Here");
+  // res.download("server.js")
+  res.render("index", { text: "World" });
+});
 
-App.listen(3000)
+const userRouter = require("./routes/users");
+
+App.use("/users", userRouter);
+
+App.listen(3000);
