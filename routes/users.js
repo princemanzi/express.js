d8f60c7f91f1ab017da233277764a8698516527a
Prince Manzi <manziprincemp@gmail.com>
2025-02-26 19:10:37 +0200

    second commit

--- routes/users.js
+++ routes/users.js
@@ -10,12 +10,32 @@ router.get("/news", (req, res) => {
   res.send("User new form");
 });
 
-router.post('/', (req, res) => {
-  res.send("New user")
+router.post("/", (req, res) => {
+  res.send("New user");
 });
 
-router.get('/:id', (req, res) => {
-  res.send(`Get user with ID: ${req.params.id}`)
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get user with ID: ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update user with ID: ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete user with ID: ${req.params.id}`);
+  });
+
+// router.get('/:id', (req, res) => {
+//   res.send(`Get user with ID: ${req.params.id}`)
+// });
+
+// router.put('/:id', (req, res) => {
+//   res.send(`Update user with ID: ${req.params.id}`)
+// });
+
+// router.delete('/:id', (req, res) => {
+//   res.send(`Delete user with ID: ${req.params.id}`)
+// });
 
 module.exports = router;
